7c27dd5f0646421131eb9ba6a2b8294ef42b33b2
Ray005 <723956891@qq.com>
2021-01-06 13:00:32 +0800

    20210106可以有空格 是的

--- sbuild&push(本文件须为GBK编码).bat
+++ sbuild&push(本文件须为GBK编码).bat
@@ -1,15 +1,17 @@
 @echo off
 ::请输入项目名称
-set /p pro=""请输入提交信息（不能含有空格）：""
+set /p pro="请输入提交信息（不能含有空格）："
 set da1=%date:~0,4%
 set da2=%date:~-8,-6%
 set da3=%date:~-5,-3%
 
-echo pro
+
 
 git add -A
 git add *
 git commit -m %da1%%da2%%da3%%pro%
 
 git push
+echo 完成
+echo %da1%%da2%%da3%%pro%
 pause
